7570be48ff4e010df9943a398858fb6d117795b9
EwoutMaertens <maertens.ewout@gmail.com>
2025-05-14 23:35:08 +1000

    nix/kitty: cleaning up kitty.conf
    Cleaning up and finishing my kitty.conf file. Also adding an initial
    kitty startup_session, which opens some of my most worked repositories.
    
    Renaming kitty-reload alias to reload-kitty.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,4 @@ alias kd="kubectl --context dev"
 alias ki="kubectl --context infra"
 
 alias hms="home-manager switch --flake '/Users/ewoutm/dotfiles#ewoutm'"
-alias kitty-reload='kill -SIGUSR1 $KITTY_PID'
+alias reload-kitty='kill -SIGUSR1 $KITTY_PID'
